296c529dc1b208521bde456a336d78ce9c8b12b4
jillinagomes <jillinagomes@192.168.2.228>
2018-01-29 17:55:08 -0500

    Started the two classes, not sure what the GUI should look like.

---
+++ src/SlotMachine.java
@@ -0,0 +1,11 @@
+
+public class SlotMachine {
+    private int payoutcycle, payoutamount, timetillnextpay;
+    
+    public SlotMachine(int c, int a, int t){
+        payoutcycle= c;
+        payoutamount= a;
+        timetillnextpay= t;
+    }
+    
+}
